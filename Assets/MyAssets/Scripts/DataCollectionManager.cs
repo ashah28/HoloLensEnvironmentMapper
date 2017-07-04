5e9e2d493d6bc6f8a177f52730e4eebb0f4ffa6f
Alankrit Shah <ashah28@users.noreply.github.com>
2017-07-04 15:57:09 -0700

    added debug manager for onscreen logs

---
+++ Assets/MyAssets/Scripts/DataCollectionManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollectionManager : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+	// Use this for initialization
+	void Start () {
+        if (!target)
+        {
+            print("No target transform found");
+        }
+        DebugManager.Instance.PrintToRunningLog("Hello");
+        DebugManager.Instance.PrintToInfoLog("World");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/MyAssets/Scripts/DebugManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+
+public class DebugManager : Singleton<DebugManager> {
+
+    [SerializeField]
+    Text runningLog;
+
+    [SerializeField]
+    Text infoLog;
+
+    public DebugManager instance;
+
+	// Use this for initialization
+	void Start () {
+        if (!runningLog)
+            Debug.LogError("Visual log not found");
+	}
+	
+    public void PrintToRunningLog(string message)
+    {
+        string data = GetCurrentTimestamp() + " : " + message + "\n";
+        print(data);
+        runningLog.text = data + runningLog.text.Substring(0, Mathf.Min(runningLog.text.Length, 1000));
+    }
+
+    public void PrintToInfoLog(string message)
+    {
+        string data = GetCurrentTimestamp() + " : " + message + "\n";
+        print(data);
+        infoLog.text = data + infoLog.text.Substring(0, Mathf.Min(infoLog.text.Length, 1000));
+    }
+
+    string GetCurrentTimestamp()
+    {
+        return string.Format("00.000", Time.time.ToString());
+    }
+}
