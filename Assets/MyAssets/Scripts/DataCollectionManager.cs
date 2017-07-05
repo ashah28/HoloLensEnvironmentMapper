8747422f59e393c0444074dad76979606928fdab
Alankrit Shah <ashah28@users.noreply.github.com>
2017-07-04 18:03:21 -0700

    Added data collection and management logic

--- Assets/MyAssets/Scripts/DataCollectionManager.cs
+++ Assets/MyAssets/Scripts/DataCollectionManager.cs
@@ -1,38 +1,112 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class DataCollectionManager : MonoBehaviour {
 
     [SerializeField]
     Transform target;
 
-    bool recording = false;
+    [SerializeField]
+    StreamWriter sr;
 
-	// Use this for initialization
-	void Start () {
+    bool recording = false;
+    string fileName = "data";
+    string filePath;
+    float recStartTime = 0;
+
+    // Use this for initialization
+    void Start () {
         if (!target)
         {
             print("No target transform found");
         }
 
+        InitFileToWrite();
+
+        if (File.Exists(filePath + ".txt"))
+            DebugManager.Instance.PrintToInfoLog("File created successfully: " + filePath);
+        else
+            DebugManager.Instance.PrintToInfoLog("Failed to create:" + filePath);
+
         //DebugManager.Instance.PrintToRunningLog("Hello");
         //DebugManager.Instance.PrintToInfoLog("World");
     }
 
+    void Update()
+    {
+        if(recording)
+            WriteToDisk();
+    }
+
+    void OnDisable()
+    {
+        if (sr != null)
+            sr.Dispose();
+    }
+
     public void OnToggleClick()
     {
-        DebugManager.Instance.PrintToInfoLog("Clicked");
+        recording = !recording;
+        DebugManager.Instance.PrintToInfoLog("Recording: " + recording);
 
-        if(recording)
+        //when the recording starts
+        if (recording)
         {
-
+            InvokeRepeating("WritePositionToScreen", 0, 0.2f);
+            recStartTime = Time.time;
         }
+        //when the recording stops
         else
         {
-
+            CancelInvoke("WritePositionToScreen");
+            if (sr != null)
+                sr.Dispose();
+            DebugManager.Instance.PrintToInfoLog("Rec. duration:" + (Time.time - recStartTime));
         }
+    }
 
-        recording = !recording;
+    void WritePositionToScreen()
+    {
+        string data = (target.position) + " :: " + target.eulerAngles;
+        DebugManager.Instance.PrintToRunningLog(data);
+    }
+
+    void InitFileToWrite()
+    {
+        try
+        {
+            string currentFileName = fileName + " " + String.Format("{0:MM-dd,hh-mm-ss}", System.DateTime.Now);
+
+            //editor
+            if (Application.isEditor)
+                filePath = Application.persistentDataPath + "/Data/" + currentFileName;
+            //deployed app
+            else
+                filePath = Application.persistentDataPath + "/" + currentFileName;
+
+            if (File.Exists(filePath + ".txt"))
+            {
+                Debug.Log(filePath + " already exists. Incrementing it.");
+                InitFileToWrite();
+            }
+            else
+            {
+                sr = File.CreateText(filePath + ".txt");
+                sr.AutoFlush = true;
+            }
+        }
+        catch(Exception e)
+        {
+            DebugManager.Instance.PrintToInfoLog("Exception:" + e);
+        }
+    }
+
+    void WriteToDisk()
+    {
+        string rowData = (Time.time - recStartTime) + " : " + transform.position + " -- " + transform.eulerAngles;
+        sr.WriteLine(rowData);
     }
 }
---
+++ Assets/MyAssets/Scripts/VideoCaptureController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.VR.WSA.WebCam;
+using UnityEngine.UI;
+
+public class VideoCaptureController : MonoBehaviour
+{
+    [SerializeField]
+    Text commonLogger;
+
+    static readonly float MaxRecordingTime = 20.0f;
+
+    VideoCapture m_VideoCapture = null;
+    float m_stopRecordingTimer = float.MaxValue;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.WSAPlayerX86)
+            StartVideoCaptureTest();
+    }
+
+    void Update()
+    {
+        if (m_VideoCapture == null || !m_VideoCapture.IsRecording)
+        {
+            return;
+        }
+
+        if (Time.time > m_stopRecordingTimer)
+        {
+            m_VideoCapture.StopRecordingAsync(OnStoppedRecordingVideo);
+        }
+    }
+
+    void StartVideoCaptureTest()
+    {
+        commonLogger.text = ("Cam Init\n") + commonLogger.text;
+        Resolution cameraResolution = VideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        commonLogger.text = (cameraResolution) + commonLogger.text;
+
+        float cameraFramerate = VideoCapture.GetSupportedFrameRatesForResolution(cameraResolution).OrderByDescending((fps) => fps).First();
+        commonLogger.text = (cameraFramerate) + commonLogger.text;
+
+        VideoCapture.CreateAsync(false, delegate (VideoCapture videoCapture)
+        {
+            if (videoCapture != null)
+            {
+                m_VideoCapture = videoCapture;
+                commonLogger.text = ("Created VideoCapture Instance!\n") + commonLogger.text;
+
+                CameraParameters cameraParameters = new CameraParameters();
+                cameraParameters.hologramOpacity = 0.0f;
+                cameraParameters.frameRate = cameraFramerate;
+                cameraParameters.cameraResolutionWidth = cameraResolution.width;
+                cameraParameters.cameraResolutionHeight = cameraResolution.height;
+                cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+                m_VideoCapture.StartVideoModeAsync(cameraParameters,
+                                                   VideoCapture.AudioState.ApplicationAndMicAudio,
+                                                   OnStartedVideoCaptureMode);
+            }
+            else
+            {
+                commonLogger.text = ("Failed to create VideoCapture Instance!\n") + commonLogger.text;
+            }
+        });
+    }
+
+    void OnStartedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
+    {
+        commonLogger.text = ("Started Video Capture Mode!\n") + commonLogger.text;
+        string timeStamp = Time.time.ToString().Replace(".", "").Replace(":", "");
+        string filename = string.Format("TestVideo_{0}.mp4", timeStamp);
+        string filepath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+        filepath = filepath.Replace("/", @"\");
+        m_VideoCapture.StartRecordingAsync(filepath, OnStartedRecordingVideo);
+    }
+
+    void OnStoppedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
+    {
+        Debug.Log("Stopped Video Capture Mode!");
+        commonLogger.text = ("Stopped Video Capture Mode!\n") + commonLogger.text;
+    }
+
+    void OnStartedRecordingVideo(VideoCapture.VideoCaptureResult result)
+    {
+        Debug.Log("Started Recording Video!");
+        commonLogger.text = ("Started Recording Video!\n") + commonLogger.text;
+        m_stopRecordingTimer = Time.time + MaxRecordingTime;
+    }
+
+    void OnStoppedRecordingVideo(VideoCapture.VideoCaptureResult result)
+    {
+        Debug.Log("Stopped Recording Video!");
+        commonLogger.text = ("Stopped Recording Video!\n") + commonLogger.text;
+        m_VideoCapture.StopVideoModeAsync(OnStoppedVideoCaptureMode);
+    }
+
+
+    public void ButtonClicked()
+    {
+        commonLogger.text = ("Button clicked\n") + commonLogger.text;
+    }
+}
